ce1d264213acd2b4be85d977de2c07b63fdd3df6
santhoshmohanr <rmohansanthosh@gmail.com>
2022-04-25 11:35:27 +0530

    My first commit

--- aexample.go
+++ aexample.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+	fmt.Println("====================================")
 	a := []int{23, 45, 67, 89, 54, 34, 24, 68}
 	for i := 0; i < len(a); i++ {
 		for j := 0; j < len(a); j++ {
